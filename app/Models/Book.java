e96a6c6b8d99aa35e64fe7c584258eeadedc9abb
robuntu <robuntu@robuntu.ca>
2018-04-20 13:06:55 -0400

    VIDEO 14

--- app/Models/Book.java
+++ app/Models/Book.java
@@ -1,4 +1,53 @@
 package Models;
 
+import java.util.*;
+
 public class Book {
+
+    public int id;
+    public String title;
+    public int price;
+    public String author;
+
+    public Book(int id, String title, int price, String author){
+
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.author = author;
+
+    }
+
+    private static Set<Book> books;
+
+    static {
+
+        books = new HashSet<>();
+        books.add(new Book(1,"C++",20,"Joseph"));
+        books.add(new Book(2,"Java",25,"Alex"));
+    }
+
+    public static Set<Book> allBooks(){
+        return books;
+    }
+
+    public static Book findById(int id){
+        for(Book book : books){
+            if(id == (book.id)){
+                return book;
+            }
+        }
+
+        return null;
+
+    }
+
+    public static void addBook(Book book){
+        books.add(book);
+    }
+
+    public static boolean remove(Book book){
+        return books.remove(book);
+    }
+
 }
--- app/controllers/BookController.java
+++ app/controllers/BookController.java
@@ -1,4 +1,41 @@
 package controllers;
 
-public class BookController {
+import Models.Book;
+import play.mvc.*;
+
+import java.util.*;
+
+import views.html.Book.index;
+
+
+public class BookController extends Controller {
+
+    public Result index(){
+        Set<Book> books = Book.allBooks();
+        return ok(index.render(Collections.singleton(books)));
+    }
+
+    public Result create(){
+        return TODO;
+    }
+
+    public Result save(){
+        return TODO;
+    }
+
+    public Result edit(Integer id){
+        return TODO;
+    }
+
+    public Result update(){
+        return TODO;
+    }
+
+    public Result destroy(Integer id){
+        return TODO;
+    }
+
+    public Result show(Integer id){
+        return  TODO;
+    }
 }
